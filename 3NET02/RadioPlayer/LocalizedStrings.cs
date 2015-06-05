c2e15e259e4dc9e8e3508b577221f509d7ac67b9
Alvin Meimoun <alvin.meimoun@gmail.com>
2015-06-05 21:03:08 +0200

    Init projet

---
+++ 3NET02/RadioPlayer/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RadioPlayer.Resources;
+
+namespace RadioPlayer
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}

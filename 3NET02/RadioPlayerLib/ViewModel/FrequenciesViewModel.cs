cce26f1cee06a4388e94b2c640b29756c1946c8d
Alvin Meimoun <alvin.meimoun@gmail.com>
2015-06-07 23:21:48 +0200

    Fréquence la plus proche

--- 3NET02/RadioPlayerLib/ViewModel/FrequenciesViewModel.cs
+++ 3NET02/RadioPlayerLib/ViewModel/FrequenciesViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Newtonsoft.Json;
+using RadioPlayerLib.Exceptions;
 using RadioPlayerLib.Resources;
 using RadioPlayerLib.Services;
 
@@ -13,16 +15,16 @@ namespace RadioPlayerLib.ViewModel
 {
     public class FrequenciesViewModel : ViewModelBase
     {
-        public bool ListLoading { get; set; }
-
         public string NearestLoadErrorMessage { get; set; }
+
+        public FrequencyViewModel NearestFrequency { get; set; }
 
         public List<FrequencyViewModel> Frequencies { get; set; } 
 
         public bool DisplayNearestLoadErrorMessage { 
             get
             {
-                return NearestLoadErrorMessage != null && NearestLoadErrorMessage.Length > 0;
+                return !string.IsNullOrEmpty(NearestLoadErrorMessage);
             } 
         }
 
@@ -36,15 +38,63 @@ namespace RadioPlayerLib.ViewModel
             }
         }
 
-        private void LoadData()
+        private async void LoadData()
         {
-            ListLoading = true;
-
+            //Reinit
+            NearestFrequency = new FrequencyViewModel
+            {
+                City = "",
+                Frequency = ""
+            };
             NearestLoadErrorMessage = ViewMessages.Loading_Position;
 
+            //Loading
             IStorageService storageService = SimpleIoc.Default.GetInstance<IStorageService>();
             var freqJsonStr = storageService.ReadFileFromProjectToString(@"Resources/frequencies.json");
             Frequencies = JsonConvert.DeserializeObject<List<FrequencyViewModel>>(freqJsonStr);
+
+            ILocationService locationService = SimpleIoc.Default.GetInstance<ILocationService>();
+            try
+            {
+                var currentPt = await locationService.getCurrentLocation();
+
+                int R = 6371; //radius de la terre
+    
+                double shortestDistance = -1;
+                FrequencyViewModel nearestFreq = null;
+    
+                for(int i = 0; i < Frequencies.Count; i++){
+                    FrequencyViewModel model = Frequencies[i];
+                        double latDistance = (model.Latitude - currentPt.Latitude) * Math.PI / 180.0;
+                        double lngDistance = (model.Longitude - currentPt.Longitude) * Math.PI / 180.0;
+
+                        double a = Math.Sin(latDistance / 2) * Math.Sin(latDistance / 2)
+                            + Math.Cos(currentPt.Latitude * Math.PI / 180.0) * Math.Cos(model.Latitude * Math.PI / 180.0)
+                        * Math.Sin(lngDistance / 2) * Math.Sin(lngDistance / 2);
+                        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+                        double distance = R * c * 1000;
+
+            
+                        if(shortestDistance == -1 || shortestDistance > distance){
+                            shortestDistance = distance;
+                            nearestFreq = model;
+                        }
+
+                }
+
+                NearestFrequency = nearestFreq;
+                RaisePropertyChanged("NearestFrequency");
+                NearestLoadErrorMessage = "";
+            }
+            catch (LocationDisabledException lde)
+            {
+                NearestLoadErrorMessage = ViewMessages.Position_Disabled;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+                NearestLoadErrorMessage = ViewMessages.Loading_Position_Error;
+            }
         }
     }
 }
